670e3fec34fe4f15d329562091a0e9a05bcd1190
HyeonJinGitHub <46698840+HyeonJinGitHub@users.noreply.github.com>
2021-07-02 15:03:48 +0900

    Project Create

--- greenfoodproject/src/main/java/net/developia/food/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package net.developia.food.service;
-
-import java.util.List;
-
-import net.developia.food.dto.MemberDTO;
-
-public interface MemberService {
-	public List<MemberDTO> getMemberList() throws Exception;
-}
---
+++ greenfoodproject/src/main/java/net/developia/greenfood/service/MemberService.java
@@ -0,0 +1,7 @@
+package net.developia.greenfood.service;
+
+import java.util.HashMap;
+
+public interface MemberService {
+	public void selectMember(HashMap<String, Object> map) throws Exception;
+}
