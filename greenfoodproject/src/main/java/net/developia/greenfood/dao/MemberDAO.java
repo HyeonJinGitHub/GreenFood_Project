f12b543b180e8bd16c1b3b35a2362785156588aa
HyeonJinGitHub <skymilk95@naver.com>
2021-07-02 22:56:27 +0900

    project delete

--- greenfoodproject/src/main/java/net/developia/greenfood/dao/MemberDAO.java
+++
@@ -1,8 +0,0 @@
-package net.developia.greenfood.dao;
-
-import java.util.HashMap;
-
-
-public interface MemberDAO {
-	public void selectMember(HashMap<String, Object> map) throws Exception;
-}
--- greenfoodproject/src/main/java/net/developia/greenfood/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package net.developia.greenfood.service;
-
-import java.util.HashMap;
-
-public interface MemberService {
-	public void selectMember(HashMap<String, Object> map) throws Exception;
-}
